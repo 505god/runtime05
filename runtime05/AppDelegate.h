72f60005ee43a2f8e6b55aafaa089e4311fc1755
qiu <18915410342@126.com>
2015-09-22 13:41:30 +0800

    Initial Commit

---
+++ runtime05/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  runtime05
+//
+//  Created by 邱成西 on 15/9/22.
+//  Copyright © 2015年 邱成西. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ runtime05/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  runtime05
+//
+//  Created by 邱成西 on 15/9/22.
+//  Copyright © 2015年 邱成西. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
